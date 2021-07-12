3f28cc4202b22c4d4b449e9936a5a0237b9f4df6
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-07-12 23:59:57 +0530

    refactor: alert message added in login

--- src/service/user.js
+++ src/service/user.js
@@ -8,12 +8,13 @@ export const userRegistration = (userCredentials) => {
         alert("You are successfully registered!!")
     }).catch((error) => {
         console.log(error);
+        alert("User already exists!!")
     })
 }
 
 export const userLogin = (loginDetails) => {
     Axios.post('/empLogin', loginDetails).then((res) => {
-        console.log(res.data.message);
+        console.log(res.data);
         alert("You are successfully logged in!!")
     }).catch((error) => {
         console.log(error);
