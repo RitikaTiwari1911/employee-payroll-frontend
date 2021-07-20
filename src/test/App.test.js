116545fa489c95ca31797a0566442631bdf86de5
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-07-20 23:36:30 +0530

    feat: added eslint

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,14 +3,13 @@
  * @file         App.test.js
  * @description  It tests App.jsx
  * @author       Ritika <spk2ritika1911@gmail.com>
- * @since        18/07/2021  
------------------------------------------------------------------------------------------------*/
+ * @since        18/07/2021
+----------------------------------------------------------------------------------------------- */
 
+import Enzyme, { shallow } from 'enzyme'
+import React from 'react'
+import App from '../App'
 
-import { shallow } from 'enzyme';
-import React from 'react';
-import App from '../App';
-import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
--- src/test/login.test.js
+++ src/test/login.test.js
@@ -3,68 +3,62 @@
  * @file         login.test.js
  * @description  It tests Login.jsx
  * @author       Ritika <spk2ritika1911@gmail.com>
- * @since        18/07/2021  
------------------------------------------------------------------------------------------------*/
+ * @since        18/07/2021
+----------------------------------------------------------------------------------------------- */
 
+import { render } from '@testing-library/react'
+import React from 'react'
+import Enzyme from 'enzyme'
+import '@testing-library/jest-dom/extend-expect'
+import Adapter from 'enzyme-adapter-react-16'
+import Login from '../pages/Login'
 
-import { render } from '@testing-library/react';
-import React from 'react';
-import Enzyme from 'enzyme';
-import '@testing-library/jest-dom/extend-expect';
-import Adapter from 'enzyme-adapter-react-16';
-import Login from '../pages/Login';
-
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 /**
  * @description test cases for login page
  */
 
 describe('Header tags test', () => {
+  it('test to see if logo is being rendered properly', () => {
+    const { getByTestId } = render(<Login />)
+    const header = getByTestId('avatar')
+    expect(header).toBeInTheDocument()
+  })
+  it('test to see if heading 1 is being rendered properly', () => {
+    const { getByTestId } = render(<Login />)
+    const title = getByTestId('heading1')
+    expect(title).toHaveTextContent('Employee Payroll Application')
+  })
 
-    it('test to see if logo is being rendered properly', () => {
-        const { getByTestId } = render(<Login />);
-        const header = getByTestId('avatar');
-        expect(header).toBeInTheDocument();
-    })
-    it('test to see if heading 1 is being rendered properly', () => {
-        const { getByTestId } = render(<Login />);
-        const title = getByTestId('heading1');
-        expect(title).toHaveTextContent("Employee Payroll Application");
-    })
+  it('test to see if heading 2 is being rendered properly', () => {
+    const { getByTestId } = render(<Login />)
+    const header = getByTestId('heading2')
+    expect(header).toHaveTextContent('Login')
+  })
+})
 
-    it('test to see if heading 2 is being rendered properly', () => {
-        const { getByTestId } = render(<Login />);
-        const header = getByTestId('heading2');
-        expect(header).toHaveTextContent('Login')
-    })
+describe('Login Form Test', () => {
+  it('check if form is being rendered properly', () => {
+    const { getByTestId } = render(<Login />)
+    const avatar = getByTestId('avatar')
+    const form = getByTestId('form')
+    const emailId = getByTestId('emailId')
+    const password = getByTestId('password')
+    const button = getByTestId('submitButton')
 
-});
+    expect(avatar).toBeInTheDocument()
+    expect(form).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+    expect(emailId).toBeInTheDocument()
+    expect(password).toBeInTheDocument()
+  })
 
-describe("Login Form Test",()=>{
+  test('check value', () => {
+    const { getByTestId } = render(<Login />)
+    const emailId = getByTestId('emailId')
+    const password = getByTestId('password')
 
-    it('check if form is being rendered properly', ()=>{
-        const { getByTestId } = render(<Login />);
-        const avatar = getByTestId('avatar');
-        const form = getByTestId('form');
-        const emailId = getByTestId('emailId');
-        const password = getByTestId('password');
-        const button = getByTestId('submitButton');
-
-        expect(avatar).toBeInTheDocument();
-        expect(form).toBeInTheDocument();
-        expect(button).toBeInTheDocument();
-        expect(emailId).toBeInTheDocument();
-        expect(password).toBeInTheDocument();
-    
-    });
-
-    test('check value', () => {
-        const { getByTestId } = render(<Login />);
-        const emailId = getByTestId('emailId');
-        const password = getByTestId('password');
-    
-        expect(emailId).toHaveTextContent('Email ID');
-        expect(password).toHaveTextContent('Password');
-    
-    });
+    expect(emailId).toHaveTextContent('Email ID')
+    expect(password).toHaveTextContent('Password')
+  })
 })
